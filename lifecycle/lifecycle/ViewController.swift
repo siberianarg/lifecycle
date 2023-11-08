3a2afee96e577b8aaec3bdce2534298e3ae87315
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2023-11-08 22:37:42 +0600

    Created lifecycle project

---
+++ lifecycle/lifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecycle
+//
+//  Created by siberianarg on 08.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
