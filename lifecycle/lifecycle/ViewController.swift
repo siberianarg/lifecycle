dbe8d0ca0224c43ce173b453265e3f34a2929da7
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2023-11-08 22:43:15 +0600

    Removed storyboard main

--- lifecycle/lifecycle/ViewController.swift
+++ lifecycle/lifecycle/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
     }
 
 
